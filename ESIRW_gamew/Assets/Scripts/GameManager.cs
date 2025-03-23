56509eb9cf05ad31b033f80a1316369dcee4db52
NuageRoue <clemenciorino@gmail.com>
2025-03-23 04:35:33 +0100

    polishing: minimap Animation

--- ESIRW_gamew/Assets/Scripts/GameManager.cs
+++ ESIRW_gamew/Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using DigitalRuby.Tween;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,6 +6,8 @@ public class GameManager : MonoBehaviour
 {
     public CharacterController characterController;
     [SerializeField] GameObject map;
+
+    [SerializeField] float duration;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -32,18 +35,48 @@ public class GameManager : MonoBehaviour
 
     void OpenMap(InputAction.CallbackContext context)
     {
+        System.Action<ITween<float>> openWindow = (t) =>
+        {
+            map.transform.localScale = new (map.transform.localScale.x, t.CurrentValue, map.transform.localScale.z);
+        };
 
-        characterController.MinimapActionMap.Enable();
-        characterController.MovementActionMap.Disable();
+        System.Action<ITween<float>> Setup = (t) =>
+        {
+            characterController.MinimapActionMap.Enable();
+        };
 
         map.SetActive(true);
         map.GetComponentInChildren<MinimapManager>().DisplayMinimap();
+        characterController.MovementActionMap.Disable();
+
+        // completion defaults to null if not passed in
+        map.Tween("MoveCircle", 0f, 1f, duration, TweenScaleFunctions.CubicEaseIn, openWindow, Setup);
+
+        
+
+        
     }
 
     void CloseMap(InputAction.CallbackContext context)
     {
-        map.gameObject.SetActive(false);
+
+        System.Action<ITween<float>> openWindow = (t) =>
+        {
+            map.transform.localScale = new(map.transform.localScale.x, t.CurrentValue, map.transform.localScale.z);
+        };
+
+        System.Action<ITween<float>> Setup = (t) =>
+        {
+            characterController.MovementActionMap.Enable();
+
+            map.SetActive(false);
+        };
+
         characterController.MinimapActionMap.Disable();
-        characterController.MovementActionMap.Enable();
+
+        // completion defaults to null if not passed in
+        map.Tween("MoveCircle", 1f, 0f, duration, TweenScaleFunctions.CubicEaseIn, openWindow, Setup);
+
+
     }
 }
